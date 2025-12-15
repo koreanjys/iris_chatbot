5ff99a67763dc32ef8a8deadfeef4444549ccbb6
koreanjys <connecttojys@gmail.com>
2025-12-15 21:34:23 +0900

    add gemini

--- irispy.py
+++ irispy.py
@@ -1,6 +1,15 @@
 
-from iris import Bot, ChatContext, IrisLink
 import sys
+import os
+
+from dotenv import load_dotenv
+from iris import Bot, ChatContext, IrisLink
+import google.generativeai as genai
+
+load_dotenv()
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY", "")
+genai.configure(api_key=GEMINI_API_KEY)
+model = genai.GenerativeModel("gemini-1.5-flash-002")
 
 if len(sys.argv) < 2:
     print("Usage: python irispy.py {아이리스HOST}:3000")  # 127.0.0.1:3000
@@ -14,9 +23,14 @@ bot = Bot(iris_url)
 @bot.on_event("message")
 def on_message(chat: ChatContext):
     if chat.room.id == 18247793138980592:  #특정 방에서만 반응
-        match chat.message.command:
-            case "!안녕":
-                chat.reply(f"안녕, {chat.sender.name}! 좋은 밤 보내!")
+        if chat.message.command.startswith("!"):
+            try:
+                prompt = "프롬프트에 유머러스하게 한국어로 답변해. 프롬프트 : " + chat.message.command[1:]  #명령어 앞의 '!' 제거
+                response = model.generate_content(prompt, max_output_tokens=500)
+                # chat.reply(f"안녕, {chat.sender.name}! 좋은 밤 보내!")
+                chat.reply(response)
+            except:
+                chat.reply("빻봇은 토큰을 모으고 있어요... 잠시 후에 다시 시도해주세요!")
 
 #입장감지
 @bot.on_event("new_member")
