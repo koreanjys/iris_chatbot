8fb4d992b3d46f82a3c03f5625e21e0289668b08
koreanjys <connecttojys@gmail.com>
2025-12-15 22:13:35 +0900

    답변수 수정

--- irispy.py
+++ irispy.py
@@ -25,7 +25,7 @@ def on_message(chat: ChatContext):
     if chat.room.id == 18247793138980592:  #특정 방에서만 반응
         if chat.message.command.startswith("!"):
             try:
-                prompt = "프롬프트에 유머러스하게 한국어로 200 자 이내로 답변해. 프롬프트 : " + chat.message.command[1:]  #명령어 앞의 '!' 제거
+                prompt = "프롬프트에 대해 유머러스하게 한국어로 100 자 이내로 짧게 답변해. 프롬프트 : " + chat.message.command[1:]  #명령어 앞의 '!' 제거
                 response = model.generate_content(prompt).text
                 # chat.reply(f"안녕, {chat.sender.name}! 좋은 밤 보내!")
                 chat.reply(response)
